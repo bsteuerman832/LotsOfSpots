4d6e9fcb71102dc75c8ddb1f2783fe5fe152a85a
Brian Steuerman <33671768+bsteuerman832@users.noreply.github.com>
2019-11-22 10:46:13 -0500

    Add files via upload

--- main/java/com/example/assignment3/Lot.java
+++ main/java/com/example/assignment3/Lot.java
@@ -6,37 +6,41 @@ public class Lot
     private String address;
     private Car[] spots;
     private int size;
-    private double lat;
-    private double lon;
+    private double dist;
+    private int carsParked;
 
-    public Lot(String theName, String theAddress, int lotSize, double latitude, double longitude)
+    public Lot(String theName, String theAddress, int lotSize, double dist_from_user)
     {
         name = theName;
         address = theAddress;
         size = lotSize;
-        lat = latitude;
-        lon = longitude;
+        dist = dist_from_user;
         spots = new Car[size];
+        carsParked = 0;
     }
 
     public int getSize() {
         return size;
     }
 
+    public Car getCarAtSpot(int spot)
+    {
+        if (spot < 0 || spot >= size)
+            return null;
+        else
+            return spots[spot];
+    }
+
     public String getAddress() {
         return address;
     }
 
-    public double getLatitude() {
-        return lat;
-    }
-
-    public double getLongitude() {
-        return lon;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public double getDist() {
+        return dist;
     }
 
     public Car[] getSpots() {
@@ -47,9 +51,29 @@ public class Lot
     {
         if (spotNum >= 0) {
             spots[spotNum] = car;
+            carsParked = carsParked + 1;
             return true;
         }
-
         return false;
+    }
+
+    public boolean checkOutCat(String licensePlate)
+    {
+        int i = 0;
+        for (i = 0; i < size; i++)
+            if (spots[i].getLicensePlate().equals(licensePlate)) {
+                spots[i] = null;
+                carsParked = carsParked - 1;
+                return true;
+            }
+        return false;
+    }
+
+    public boolean isFull()
+    {
+        if (size - carsParked == 0)
+            return true;
+        else
+            return false;
     }
 }
